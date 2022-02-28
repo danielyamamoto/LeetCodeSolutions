8c1bf1cc08ffbbaf7a22ce3092945de8cbbd5bd0
DanielYamamoto <danylarayamamoto@hotmail.com>
2022-02-28 16:25:31 -0600

    0013 Problem

---
+++ 0013 - Roman to Integer/README.md
@@ -0,0 +1,7 @@
+#### [Roman to Integer](https://leetcode.com/problems/roman-to-integer/)
+
+Difficulty: Easy
+
+| Time Submitted | Runtime | Memory |
+|----------------|---------|--------|
+| 09/30/2021     | 8 ms    | 6 MB   |
---
+++ 0013 - Roman to Integer/main.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int value(char roman) {
+        switch(roman) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+    
+    int romanToInt(string s) {
+        int result = 0;
+        
+        for (int i = s.size()-1; i >=0; i--) {
+            if (value(s[i]) >= value(s[i+1])) {
+                result += value(s[i]);
+            } else {
+                result -= value(s[i]);
+            }
+        }        
+        
+        return result;
+    }
+};
